cb674dd20b1e5a99e1428dffa06372454ad51f08
taoguangc <taoguangc@gmail.com>
2023-09-26 10:25:17 +0800

    add swup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,18 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import alpinejs from '@astrojs/alpinejs'
+import swup from '@swup/astro'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), alpinejs()]
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    alpinejs(),
+    swup({
+      theme: ['slide'],
+    }),
+  ],
 })
