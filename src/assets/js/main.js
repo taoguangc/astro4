8360cc1569162c5389c1a1f9d135c3f41193d3de
taoguangc <taoguangc@gmail.com>
2023-10-05 09:40:59 +0800

    update lenis

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,11 +1,14 @@
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import Lenis from '@studio-freight/lenis'
+gsap.registerPlugin(ScrollTrigger)
 
 const lenis = new Lenis({
   duration: 1.2,
   smoothWheel: true,
 })
+
+lenis.on('scroll', ScrollTrigger.update)
 
 function raf(time) {
   lenis.raf(time)
@@ -13,10 +16,15 @@ function raf(time) {
 }
 requestAnimationFrame(raf)
 
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000)
+})
+
+gsap.ticker.lagSmoothing(0)
+
 document.getElementById('totop').addEventListener('click', () => {
   lenis.scrollTo('top', { duration: 1.5 })
 })
 
-gsap.registerPlugin(ScrollTrigger)
 window.gsap = gsap
 window.ScrollTrigger = ScrollTrigger
