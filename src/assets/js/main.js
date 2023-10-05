5a43a12f9827d2a6051a7ecacc88998b813fa9e1
taoguangc <taoguangc@gmail.com>
2023-10-05 09:27:15 +0800

    modify main.js

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,9 +1,5 @@
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
-gsap.registerPlugin(ScrollTrigger)
-window.gsap = gsap
-window.ScrollTrigger = ScrollTrigger
-
 import Lenis from '@studio-freight/lenis'
 
 const lenis = new Lenis({
@@ -21,5 +17,6 @@ document.getElementById('totop').addEventListener('click', () => {
   lenis.scrollTo('top', { duration: 1.5 })
 })
 
-// var usrlang = navigator.language
-// console.log(usrlang)
+gsap.registerPlugin(ScrollTrigger)
+window.gsap = gsap
+window.ScrollTrigger = ScrollTrigger
