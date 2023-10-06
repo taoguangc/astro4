64ac103c3a3feea777f4aab4082ce9e85c1ffec2
taoguangc <taoguangc@gmail.com>
2023-10-06 17:01:16 +0800

    enable lenis

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,30 +1,30 @@
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
-// import Lenis from '@studio-freight/lenis'
+import Lenis from '@studio-freight/lenis'
 gsap.registerPlugin(ScrollTrigger)
 
-// const lenis = new Lenis({
-//   // duration: 1.2,
-//   // smoothWheel: true,
-// })
+const lenis = new Lenis({
+  // duration: 1.2,
+  // smoothWheel: true,
+})
 
 // lenis.on('scroll', ScrollTrigger.update)
 
-// // function raf(time) {
-// //   lenis.raf(time)
-// //   requestAnimationFrame(raf)
-// // }
-// // requestAnimationFrame(raf)
+// function raf(time) {
+//   lenis.raf(time)
+//   requestAnimationFrame(raf)
+// }
+// requestAnimationFrame(raf)
 
-// gsap.ticker.add((time) => {
-//   lenis.raf(time * 1000)
-// })
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000)
+})
 
-// gsap.ticker.lagSmoothing(0)
+gsap.ticker.lagSmoothing(0)
 
-// document.getElementById('totop').addEventListener('click', () => {
-//   lenis.scrollTo('top', { duration: 1.5 })
-// })
+document.getElementById('totop').addEventListener('click', () => {
+  lenis.scrollTo('top', { duration: 1.5 })
+})
 
 window.gsap = gsap
 window.ScrollTrigger = ScrollTrigger
