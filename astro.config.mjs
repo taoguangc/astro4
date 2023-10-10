8f4ddfd409f3b620a5f6b2b5a992c797b517141b
taoguangc <taoguangc@gmail.com>
2023-10-10 14:30:19 +0800

    pnpx astro add astro-robots-txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,12 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
-import alpinejs from '@astrojs/alpinejs'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import alpinejs from '@astrojs/alpinejs';
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind(),
-    alpinejs(),
-  ],
-})
+  integrations: [mdx(), sitemap(), tailwind(), alpinejs(), robotsTxt()]
+});
