fb8edac0ba7303bcf2832535bba61fdb035178fb
taoguangc <taoguangc@gmail.com>
2024-07-26 23:00:44 +0800

    add adapter cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,18 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import alpinejs from '@astrojs/alpinejs';
-import robotsTxt from 'astro-robots-txt';
-import react from '@astrojs/react';
-import markdoc from '@astrojs/markdoc';
-import keystatic from '@keystatic/astro';
-
-import cloudflare from "@astrojs/cloudflare";
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import alpinejs from '@astrojs/alpinejs'
+import robotsTxt from 'astro-robots-txt'
+import react from '@astrojs/react'
+import markdoc from '@astrojs/markdoc'
+import keystatic from '@keystatic/astro'
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), tailwind(), alpinejs(), robotsTxt(), react(), markdoc(), keystatic()],
   output: 'hybrid',
-  adapter: cloudflare()
-});
+  adapter: cloudflare(),
+})
